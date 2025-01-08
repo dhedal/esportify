51812bb2b9f987f7a6a22da87fbda67298bf6185
dhedal <hedgardavid@yahoo.fr>
2025-01-08 11:20:14 +0100

    commit initial: spring boot project setup

---
+++ src/main/java/com/esportify/EsportifyApplication.java
@@ -0,0 +1,13 @@
+package com.esportify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsportifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsportifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/esportify/EsportifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esportify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsportifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
