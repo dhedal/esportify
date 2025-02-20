1b4c8b541b9a969b26ee7b4696d71e32f32404f8
dhedal <hedgardavid@yahoo.fr>
2025-02-20 15:59:24 +0100

    mis en commentaire de la fonction autoConnect car utile que pour le développement

--- src/main/java/com/esportify/controller/HomeController.java
+++ src/main/java/com/esportify/controller/HomeController.java
@@ -37,7 +37,7 @@ public class HomeController extends BaseController{
 
     @GetMapping("/home")
     public String home(Model model, @AuthenticationPrincipal User user) {
-        this.autoConnect(user);
+//        this.autoConnect(user);
         LOG.debug("## home(Model model)");
         this.addAttributes(model, user);
         List<EventDTO> events = this.eventService.getUpcomingAndOngoingEvents();
@@ -46,31 +46,31 @@ public class HomeController extends BaseController{
         return "home";
     }
 
-    public User autoConnect(User user) {
-        if(user != null) return user;
-        try {
-
-            // 🔥 Créer une requête de connexion
-            LoginRequest loginRequest = new LoginRequest();
-            loginRequest.setEmail("admin@esportify.com");
-            loginRequest.setPassword("StrongPassword!1");
-
-            // 🔥 Exécuter l'authentification
-            LoginResponse response = new LoginResponse();
-            authenticationService.authenticate(loginRequest, response);
-
-            if (response.isOk()) {
-                System.out.println("Connexion automatique réussie pour admin@esportify.com !");
-            } else {
-                System.out.println("Échec de la connexion automatique : " + response.getMessages());
-            }
-
-
-        } catch (Exception e) {
-            System.out.println("Impossible de connecter automatiquement l'utilisateur.");
-            e.printStackTrace();
-        }
-        return user;
-    }
+//    public User autoConnect(User user) {
+//        if(user != null) return user;
+//        try {
+//
+//            // 🔥 Créer une requête de connexion
+//            LoginRequest loginRequest = new LoginRequest();
+//            loginRequest.setEmail("admin@esportify.com");
+//            loginRequest.setPassword("StrongPassword!1");
+//
+//            // 🔥 Exécuter l'authentification
+//            LoginResponse response = new LoginResponse();
+//            authenticationService.authenticate(loginRequest, response);
+//
+//            if (response.isOk()) {
+//                System.out.println("Connexion automatique réussie pour admin@esportify.com !");
+//            } else {
+//                System.out.println("Échec de la connexion automatique : " + response.getMessages());
+//            }
+//
+//
+//        } catch (Exception e) {
+//            System.out.println("Impossible de connecter automatiquement l'utilisateur.");
+//            e.printStackTrace();
+//        }
+//        return user;
+//    }
 
 }
