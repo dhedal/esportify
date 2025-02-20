64b2d2625412c7820eeae0b6712adf933a53c372
dhedal <hedgardavid@yahoo.fr>
2025-02-20 17:56:48 +0100

    correction de l'encodage de l'application.properties en UTF-8

--- src/main/java/com/esportify/EsportifyApplication.java
+++ src/main/java/com/esportify/EsportifyApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EsportifyApplication{
-
     public static void main(String[] args) {
         SpringApplication.run(EsportifyApplication.class, args);
     }
